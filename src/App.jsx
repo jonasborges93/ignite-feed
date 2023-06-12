f7c377b16226a2d98d0897a447ed197656640314
jonasborges93 <jonas.bgs93@gmail.com>
2023-06-12 10:19:00 -0300

    ✨ Added dynamic content to the POST component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import { Post } from "./components/Post/Post";
 import './global.css';
 import styles from './App.module.css'
 
+const posts = [
+  {
+    id: 1,
+    author:{
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content:[
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-06-11 20:00:00'),
+  },
+  {
+    id: 2,
+    author:{
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content:[
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-06-10 20:00:00'),
+  },
+]
+
 function App() {
   return (
     <>
@@ -14,8 +45,16 @@ function App() {
         <div className={styles.container}>
           <Sidebar />
           <main>
-            <Post />
-            <Post />
+            { posts.map( post => {
+              return (
+                <Post 
+                  key={post.id}
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })}
           </main>
         </div>
       </section>
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,31 +1,55 @@
+/* eslint-disable react/prop-types */
+
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Avatar } from '../Avatar/Avatar'
 import { Comment } from '../Comment/Comment'
 import styles from './Post.module.css'
 
-export function Post () {
+export function Post ({author, content, publishedAt}) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'",{
+    locale: ptBR
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt,{
+    locale: ptBR,
+    addSuffix: true
+  })
+
   return (
     <article className={styles.post}>
       <header className={styles.header}>
         <div className={styles.author}>
-          <Avatar src="https://github.com/jonasborges93.png"/>
+          <Avatar src={author.avatarUrl}/>
           <div className={styles.authorInfo}>
-            <strong>Jonas Borges</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
         <time 
-          title='06 de Junho de 2023 às 09:24h' 
-          dateTime='2023-06-06 09:24:00' 
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()} 
         >
-          Publicado a 1h
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <a href='#'>jane.design/doctorcare</a>
+        {content.map(line => {
+          if (line.type === 'paragraph'){
+            return (
+              <p key={line.index}>{line.content}</p>
+            )
+          } else if (line.type === 'link'){
+            return (
+              <p key={line.index}>
+                <a href='#'>{line.content}</a>
+              </p>
+            )
+          }
+        })}
         <div className={styles.hastags}>
           <a href="#">#novoprojeto</a> {' '}
           <a href="#">#nlw</a> {' '}
